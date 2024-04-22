eed12a20f2c981ab936894b05ef931d85d731ef3
Hemanth-Thaluru <thaluruhemanth105@gmail.com>
2024-04-22 13:49:29 -0400

    intial commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/hemanththaluru/JOB_Projects/nextjs_folio/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
